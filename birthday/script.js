561704f845cd2e88892809dad77956a1a11ef706
Kimberly <kimolanga123@gmail.com>
2022-03-05 23:31:15 +0300

    added form id

--- birthday/script.js
+++ birthday/script.js
@@ -3,4 +3,11 @@ function displayYear() {
     alert(year);
 }
 
-
+function displayMonth() {
+    var month = document.getElementById("month").value;
+    alert(month);
+}
+function displayDay(){
+    var day = document.getElementById("day").value;
+    alert(day);
+}
